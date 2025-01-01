ad832abc062802198b6f55f7f96758d3f17bbef3
Shatodru <shatodru2003@gmail.com>
2025-01-02 01:54:49 +0530

    2/01/2025

--- 01_basic/first.py
+++ 01_basic/first.py
@@ -1,1 +1,7 @@
 print("Chai and python")
+
+def chai(n): # a function called chai is defined here with a value of "n"
+    print(n)
+chai(4) # 4 would simply be printed
+chai("lemon tea") # lemon tea (a string) would be printed.
+#Now the function called chai would be exported from this file to the file called second.py, so check that out.

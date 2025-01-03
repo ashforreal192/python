6f644d9c4941e97cda91c6b031f85a38b88ca367
Shatodru <shatodru2003@gmail.com>
2025-01-04 00:06:41 +0530

    4/01/2025

--- 01_basic/dataTypes.md
+++ 01_basic/dataTypes.md
@@ -4,7 +4,7 @@
 - String: 'spam', "Bob's", b'a\x01c', u'sp\xc4m'
 - List: [1, 'three', 4.5], list(range(10))
 - Tuple: (1, 'spam', 4, 'U'), tuple('spam'), namedtuple
-- Dictionary: {'food': 'spam', 'taste': 'yum'}, dict(hours=10)
+- Dictionary: {'food':'spam', 'taste':'yum'}, dict(hours=10)
 - Set: set('abc'), {'a', 'b', 'c'}
 - File: open('eggs.txt'), open(r'C:\ham.bin', 'wb')
 - Boolean: True, False
--- 01_basic/first.py
+++ 01_basic/first.py
@@ -8,6 +8,8 @@ chai("lemon tea") # lemon tea (a string) would be printed.
 
 chai_one = "ginger tea"
 chai_two = "masala chai"
-# important: I entered the above two variables after I imported python in my terminal. But the python shell doesn't get reloaded automatically so even after pressing ctrl+s,
-# it didn't get saved/reloaded. So for the purpose of new entries being reloaded, we use a command called "from importlib import reload". And then write "reload(filename)". In
+# important: I entered the above two variables after I imported python in my terminal. But the python shell
+# doesn't get reloaded automatically so even after pressing ctrl+s,
+# it didn't get saved/reloaded. So for the purpose of new entries being reloaded,
+# we use a command called "from importlib import reload". And then write "reload(filename)". In
 # this case, reload(first). After that we can access the new variables using command....  first.chai_one
